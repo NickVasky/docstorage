3811bcbddcac680e1b756eb84a419a3d572077f1
Nikita Vasilev <nickvasky@gmail.com>
2025-09-05 10:12:39 +0300

    Models added for docs metadata

---
+++ internal/models/models.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type DocumentMetadata struct {
+	ID        uuid.UUID
+	Name      string
+	IsFile    bool
+	MimeType  string
+	IsPublic  bool
+	CreatedAt time.Time
+	URL       string
+}
+
+type Document struct {
+	DocumentMetadata
+	File os.File
+}
